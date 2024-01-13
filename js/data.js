73772d63e18602ab672397f14f1e00c3ead7b4a3
Seraf-seraf <serafim123sima@mail.ru>
2024-01-13 15:26:35 +0600

    build

--- js/data.js
+++
@@ -1,9 +0,0 @@
-const createUser = ({id, description, url, likes, comments}) => ({
-  id: id,
-  description: description,
-  likes: likes,
-  comments: comments,
-  url: url,
-});
-
-export { createUser };
